f4f2c0fec6144f716e6463a269151c0f7a986076
Isabela Maria Da Campo <isabeladacampo@gmail.com>
2022-04-20 15:37:05 -0300

    keyboard better way to avoid view

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView } from 'react-native';
 import { format } from 'date-fns';
 
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
 import { Button } from '../components/Button';
+import { KeyboardSpacer } from '../components/KeyboardSpacer';
 
 const screen = Dimensions.get('window');
 
@@ -52,21 +53,6 @@ export default () => {
 
   const [scrollEnabled, setScrollEnabled] = useState(false);
 
-  useEffect(() => {
-    const showListener = Keyboard.addListener('keyboardDidShow', () => {
-      setScrollEnabled(true);
-    });
-
-    const hideListener = Keyboard.addListener('keyboardDidHide', () => {
-      setScrollEnabled(false);
-    });
-
-    return () => {
-      showListener.remove();
-      hideListener.remove();
-    };
-  }, []);
-
   return (
     <View style={styles.container}>
       <ScrollView scrollEnabled={scrollEnabled}>
@@ -105,7 +91,8 @@ export default () => {
             {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}.`}
           </Text>
           <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
-          <View style={{ height: screen.height }} />
+          <KeyboardSpacer onToggle={visible => setScrollEnabled(visible)} />
+
         </View>
       </ScrollView>
     </View>

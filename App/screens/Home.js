84ff84bc0aa1a417a24cbd1aa67139d740b9ad97
Isabela Maria Da Campo <isabeladacampo@gmail.com>
2022-04-20 14:57:38 -0300

    simple way to keyboard avoid view

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView, Keyboard } from 'react-native';
 import { format } from 'date-fns';
 
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
+import { Button } from '../components/Button';
 
 const screen = Dimensions.get('window');
 
@@ -11,7 +12,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.blue,
-    justifyContent: 'center',
+  },
+  content: {
+    paddingTop: screen.height * 0.2,
   },
   logoContainer: {
     alignItems: 'center',
@@ -47,41 +50,64 @@ export default () => {
   const conversionRate = 0.8345;
   const date = new Date();
 
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  useEffect(() => {
+    const showListener = Keyboard.addListener('keyboardDidShow', () => {
+      setScrollEnabled(true);
+    });
+
+    const hideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setScrollEnabled(false);
+    });
+
+    return () => {
+      showListener.remove();
+      hideListener.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
-      <View style={styles.logoContainer}>
-        <Image 
-          source={require('../assets/images/background.png')}
-          style={styles.logoBackground}
-          resizeMode="contain"
-        />
-        <Image 
-          source={require('../assets/images/logo.png')}
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </View>
+      <ScrollView scrollEnabled={scrollEnabled}>
+        <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image 
+              source={require('../assets/images/background.png')}
+              style={styles.logoBackground}
+              resizeMode="contain"
+            />
+            <Image 
+              source={require('../assets/images/logo.png')}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+          </View>
 
-      <Text style={styles.textHeader}>Currency Converter</Text>
+          <Text style={styles.textHeader}>Currency Converter</Text>
 
-      <ConversionInput 
-        text="USD"
-        value="123"
-        onButtonPress={() => alert('Button pressed')}
-        onChangeText={text => console.log('text', text)}
-        keyboardType="numeric"
-      />
-      <ConversionInput 
-        text="GBP"
-        value="123"
-        editable={false}
-        onButtonPress={() => alert("todo!")}
-      />
+          <ConversionInput 
+            text="USD"
+            value="123"
+            onButtonPress={() => alert('Button pressed')}
+            onChangeText={text => console.log('text', text)}
+            keyboardType="numeric"
+          />
+          <ConversionInput 
+            text="GBP"
+            value="123"
+            editable={false}
+            onButtonPress={() => alert("todo!")}
+          />
 
-      <Text style={styles.text}>
-        {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}.`}
-      </Text>
+          <Text style={styles.text}>
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}.`}
+          </Text>
+          <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
+          <View style={{ height: screen.height }} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
